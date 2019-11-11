5eebca6bd02e0a31529cb201af7c6a8ed0b08c0d
toastedfrog <toastedfrog@yahoo.com>
2019-11-10 22:06:31 -0800

    First commit, basic server

---
+++ server.js
@@ -0,0 +1,13 @@
+//server.js
+const http = require('http'),
+	server = http.createServer();
+
+server.on('request',(request,response)=>{
+	response.writeHead(200,{'Content-Type':'text/plain'});
+	response.write('Hello world');
+	response.end();
+});
+
+server.listen(3000, ()=>{
+	console.log('Node server created at port 3000');
+});
