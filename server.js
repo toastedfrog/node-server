1eef3c5b772e0d3a3001b4e9a00766500794c958
toastedfrog <toastedfrog@yahoo.com>
2019-11-11 21:05:10 -0800

    Granular server initialization

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 //server.js
 const http = require('http'),
-	server = http.createServer();
 
-server.on('request',(request,response)=>{
+makeServer = function (request,response){
 	response.writeHead(200,{'Content-Type':'text/plain'});
 	response.write('Hello world');
 	response.end();
-});
+},
+
+server = http.createServer(makeServer);
 
 server.listen(3000, ()=>{
 	console.log('Node server created at port 3000');
